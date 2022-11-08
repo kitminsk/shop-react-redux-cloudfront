058172df8dee09e327b44d228e409d27d1d2153b
kitminsk <kitminsk@gmail.com>
2022-11-08 18:52:09 +0100

    Add import path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://gsjh4sve5i.execute-api.us-east-1.amazonaws.com/dev/",
   bff: "https://9hqiw33gmi.execute-api.us-east-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
