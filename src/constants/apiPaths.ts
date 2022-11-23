298ea3f84a7b52693274d10bf5fedc616ccb2b6c
kitminsk <kitminsk@gmail.com>
2022-11-23 16:22:51 +0100

    Task 6: update paths, uncomment csvfile parser

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://uz3oj4j2dl.execute-api.us-east-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://gsjh4sve5i.execute-api.us-east-1.amazonaws.com/dev/",
+  import: "https://uz3oj4j2dl.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://9hqiw33gmi.execute-api.us-east-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://uz3oj4j2dl.execute-api.us-east-1.amazonaws.com",
 };
 
 export default API_PATHS;
